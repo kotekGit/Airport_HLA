0ef653236ff294d63a6046bc2886b93ab19d7183
Łukasz Kotowski <no25@o2.pl>
2014-06-25 18:35:46 +0200

    Stacja meteorologiczne, początkowa inicjazlizacja.

--- Airport_HLA/src/pl/edu/wat/wcy/mtsk/lotnisko/start/Samolot.java
+++ Airport_HLA/src/pl/edu/wat/wcy/mtsk/lotnisko/start/Samolot.java
@@ -6,26 +6,22 @@ import pl.edu.wat.wcy.mtsk.lotnisko.federaci.SamolotFederat;
 
 /**
  * Uruchomienie federacji samolotu
+ * 
  * @author mariusz
- *
+ * 
  */
 public class Samolot {
-	public static void main(String[] args) {
-		String federateName = "samolotFederate";
-		if( args.length != 0 )
-		{
-			federateName = args[0];
-		}
-		
-		try
-		{
-			// run the example federate
-			new SamolotFederat(federateName).runFederate(new SamolotAmbasador() );
-		}
-		catch( RTIexception rtie )
-		{
-			// an exception occurred, just log the information and exit
-			rtie.printStackTrace();
-		}
+    public static void main(String[] args) {
+	String federateName = "Samolot: JEDYNAKI Airline's";
+	if (args.length != 0) {
+	    federateName = args[0];
 	}
+
+	try {
+	    new SamolotFederat(federateName)
+		    .runFederate(new SamolotAmbasador());
+	} catch (RTIexception rtie) {
+	    rtie.printStackTrace();
+	}
+    }
 }
